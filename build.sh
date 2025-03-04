49d26aa0eefb9b35201e66826ce62d48d68f6566
Johnathan Spiva <sprungles.me@tuta.io>
2025-03-03 19:25:26 -0800

    Changed repository over to testing repository

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ if [ "ls /usr/bin | grep mkarchiso" != "mkarchiso" ] ; then
    sudo pacman -Syy archiso --noconfirm
 fi  
 
-rm -rf work/ && rm -rf out/ && sudo '/usr/bin/mkarchiso'  -L AcreetionOS-GNOME-with-OpenZFS -v -o ../ISO . -j$nproc && sudo rm -rf ./work
+rm -rf work/ && rm -rf out/ && sudo '/usr/bin/mkarchiso'  -L AcreetionOS-GNOME-with-OpenZFS -v -o ../ISO . -j12 && sudo rm -rf ./work
 
 
